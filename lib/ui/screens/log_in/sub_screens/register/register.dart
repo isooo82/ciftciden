8d479c7f14f093b0d7e9c13b2febe24d01cff847
Mehmet Semih BABACAN <55881663+koltukutsu@users.noreply.github.com>
2023-05-05 01:16:26 +0300

    Add: added register screen ui

--- lib/ui/screens/log_in/sub_screens/register/register.dart
+++ lib/ui/screens/log_in/sub_screens/register/register.dart
@@ -1,4 +1,7 @@
+import 'package:ciftciden/ui/common_widgets/atoms/ciftciden_text_field.dart';
+import 'package:ciftciden/ui/common_widgets/organisms/upper_place_holder.dart';
 import 'package:flutter/material.dart';
+import 'package:sizer/sizer.dart';
 
 class RegisterScreen extends StatefulWidget {
   const RegisterScreen({Key? key}) : super(key: key);
@@ -11,7 +14,32 @@ class _RegisterScreenState extends State<RegisterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+      body: Container(
+        child:
+        Column(
+          children: [
+            const UpperPlaceHolder(),
+            Padding(
+              padding: EdgeInsets.symmetric(vertical: 5.h,horizontal: 5.w),
+              child: Column(
+                children: const [
+                  CiftcidenTextField(icon: Icons.message_sharp, text: "Telefon Numarası"),
+                  CiftcidenTextField(icon: Icons.message_sharp, text: "Mail Adresi"),
+                  CiftcidenTextField(icon: Icons.message_sharp, text: "İkamet Adresi"),
+                  CiftcidenTextField(icon: Icons.message_sharp, text: "Parola"),
+                  CiftcidenTextField(icon: Icons.message_sharp, text: "Parola Doğrula"),
+                ],
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Login Butonuna Tıklanınca Yapılacak İşlemler
+              },
+              child: const Text('KAYDOL'),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
